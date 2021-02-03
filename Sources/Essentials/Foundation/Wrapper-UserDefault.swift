7a25324f17f469736c1afd2b1ef6dbcb9bcf2c31
Vasilis Akoinoglou <alladinian@gmail.com>
2021-02-03 14:50:48 +0200

    UserDefault property wrapper

---
+++ Sources/Essentials/Foundation/Wrapper-UserDefault.swift
@@ -0,0 +1,28 @@
+//
+//  File.swift
+//  
+//
+//  Created by Vasilis Akoinoglou on 3/2/21.
+//
+
+import Foundation
+
+@propertyWrapper
+public struct UserDefault<T> {
+    let key: String
+    let defaultValue: T
+
+    public init(_ key: String, defaultValue: T) {
+        self.key = key
+        self.defaultValue = defaultValue
+    }
+
+    public var wrappedValue: T {
+        get {
+            UserDefaults.standard.object(forKey: key) as? T ?? defaultValue
+        }
+        set {
+            UserDefaults.standard.set(newValue, forKey: key)
+        }
+    }
+}
