71d1977a5f90ccd9a275b920a32739a7855dede2
Vasilis Akoinoglou <alladinian@gmail.com>
2021-02-07 13:05:24 +0200

    Image & NavigationItem utils

--- Sources/Essentials/UIKit/Extensions/UIImage.swift
+++ Sources/Essentials/UIKit/Extensions/UIImage.swift
@@ -10,16 +10,37 @@ import UIKit
 
 public extension UIImage {
 
+    convenience init(view: UIView) {
+        UIGraphicsBeginImageContext(view.frame.size)
+        view.layer.render(in: UIGraphicsGetCurrentContext()!)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        self.init(cgImage: image!.cgImage!)
+    }
+
+    convenience init(color: UIColor) {
+        let size: CGSize = CGSize(width: 1, height: 1)
+        UIGraphicsBeginImageContextWithOptions(size, false, 0)
+        let context = UIGraphicsGetCurrentContext()!
+        context.setBlendMode(.normal)
+        let imageRect = CGRect(x: 0, y: 0, width: size.width, height: size.height)
+        color.setFill()
+        context.fill(imageRect)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        self.init(cgImage: image!.cgImage!)
+    }
+
     func withColor(_ color: UIColor) -> UIImage? {
         let maskImage = cgImage!
 
-        let width = size.width
-        let height = size.height
-        let bounds = CGRect(x: 0, y: 0, width: width, height: height)
+        let width      = size.width
+        let height     = size.height
+        let bounds     = CGRect(x: 0, y: 0, width: width, height: height)
 
         let colorSpace = CGColorSpaceCreateDeviceRGB()
         let bitmapInfo = CGBitmapInfo(rawValue: CGImageAlphaInfo.premultipliedLast.rawValue)
-        let context = CGContext(data: nil, width: Int(width), height: Int(height), bitsPerComponent: 8, bytesPerRow: 0, space: colorSpace, bitmapInfo: bitmapInfo.rawValue)!
+        let context    = CGContext(data: nil, width: Int(width), height: Int(height), bitsPerComponent: 8, bytesPerRow: 0, space: colorSpace, bitmapInfo: bitmapInfo.rawValue)!
 
         context.clip(to: bounds, mask: maskImage)
         context.setFillColor(color.cgColor)
@@ -30,6 +51,22 @@ public extension UIImage {
         } else {
             return nil
         }
+    }
+
+    func withColor2(_ color: UIColor) -> UIImage {
+        guard let cgImage = self.cgImage else { return self }
+        UIGraphicsBeginImageContextWithOptions(size, false, 0)
+        guard let context = UIGraphicsGetCurrentContext() else { return self }
+        context.translateBy(x: 0, y: size.height)
+        context.scaleBy(x: 1.0, y: -1.0)
+        context.setBlendMode(.normal)
+        let imageRect = CGRect(x: 0, y: 0, width: size.width, height: size.height)
+        context.clip(to: imageRect, mask: cgImage)
+        color.setFill()
+        context.fill(imageRect)
+        guard let newImage = UIGraphicsGetImageFromCurrentImageContext() else { return self }
+        UIGraphicsEndImageContext()
+        return newImage
     }
 
     func withAlpha(_ value:CGFloat) -> UIImage {
--- Sources/Essentials/UIKit/Extensions/UINavigationItem.swift
+++ Sources/Essentials/UIKit/Extensions/UINavigationItem.swift
@@ -47,6 +47,64 @@ public extension UINavigationItem {
         self.titleView = stackView
     }
 
+    func setTitle(title: String?, subtitle: String?, font: UIFont? = nil, image: UIImage? = nil) {
+        let one = UILabel()
+        one.text = title
+        if let font = font {
+            one.font = font
+        }
+        one.adjustsFontSizeToFitWidth = true
+        one.textAlignment             = .center
+        one.setContentCompressionResistancePriority(.required, for: .vertical)
+        one.sizeToFit()
+
+        if subtitle?.isEmpty == true {
+            self.titleView = one
+            return
+        }
+
+        let two = UILabel()
+        two.text = subtitle
+        if let font = font {
+            two.font = font
+        }
+        two.adjustsFontSizeToFitWidth = true
+        two.minimumScaleFactor        = 0.8
+        two.lineBreakMode             = .byTruncatingTail
+        two.textAlignment             = .center
+        two.setContentCompressionResistancePriority(.required, for: .vertical)
+        two.sizeToFit()
+
+        let stackView = UIStackView(arrangedSubviews: [one, two])
+        stackView.distribution = .equalCentering
+        stackView.axis         = .vertical
+
+        let width       = max(one.frame.size.width, two.frame.size.width)
+        stackView.frame = CGRect(x: 0, y: 0, width: width, height: 35)
+
+        one.sizeToFit()
+        two.sizeToFit()
+
+        guard let image = image else {
+            self.titleView = stackView
+            return
+        }
+
+        let iv = UIImageView(image: image)
+        let sp = UIView()
+        let w = stackView.bounds.height - 12
+        iv.translatesAutoresizingMaskIntoConstraints = false
+        iv.widthAnchor.constraint(equalToConstant: w).isActive = true
+        iv.contentMode = .scaleAspectFit
+        sp.widthAnchor.constraint(equalToConstant: w).isActive = true
+        let hstack = UIStackView(arrangedSubviews: [iv, stackView, sp])
+        hstack.distribution = .equalCentering
+        hstack.axis         = .horizontal
+        hstack.spacing = 8
+        self.titleView = hstack
+
+    }
+
 }
 
 #endif
