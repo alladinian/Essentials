1a17ea1050b21e85719a3c120410f8ccc94be49f
Vasilis Akoinoglou <alladinian@gmail.com>
2021-03-22 13:30:50 +0200

    UIStackView extension

---
+++ Sources/Essentials/UIKit/Extensions/UIStackView.swift
@@ -0,0 +1,27 @@
+//
+//  File.swift
+//  
+//
+//  Created by Vasilis Akoinoglou on 22/3/21.
+//
+
+import UIKit
+
+public extension UIStackView {
+
+    func removeAllArrangedSubviews() {
+        arrangedSubviews.forEach { removeViewAndConstraints($0) }
+    }
+
+    func replaceArrangedSubviewsWith(_ views: [UIView]) {
+        removeAllArrangedSubviews()
+        views.forEach { addArrangedSubview($0) }
+    }
+
+    private func removeViewAndConstraints(_ view: UIView) {
+        removeArrangedSubview(view)
+        NSLayoutConstraint.deactivate(view.constraints)
+        view.removeFromSuperview()
+    }
+
+}
