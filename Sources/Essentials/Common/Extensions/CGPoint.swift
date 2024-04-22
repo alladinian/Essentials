111b3fcb9f5ea8465ecdf59079d136348e556dbe
Vasilis Akoinoglou <alladinian@gmail.com>
2024-04-22 09:19:05 +0300

    Bump

--- Sources/Essentials/Common/Extensions/CGPoint.swift
+++ Sources/Essentials/Common/Extensions/CGPoint.swift
@@ -12,6 +12,22 @@ public func + (lhs: CGPoint, rhs: CGSize) -> CGPoint {
     CGPoint(x: lhs.x + rhs.width, y: lhs.y + rhs.height)
 }
 
+public func + (lhs: CGPoint, rhs: CGPoint) -> CGPoint {
+    CGPoint(x: lhs.x + rhs.x, y: lhs.y + rhs.y)
+}
+
+public func - (lhs: CGPoint, rhs: CGPoint) -> CGPoint {
+    CGPoint(x: lhs.x - rhs.x, y: lhs.y - rhs.y)
+}
+
+public func + (lhs: CGPoint, rhs: CGFloat) -> CGPoint {
+    CGPoint(x: lhs.x + rhs, y: lhs.y + rhs)
+}
+
+public func - (lhs: CGPoint, rhs: CGFloat) -> CGPoint {
+    CGPoint(x: lhs.x - rhs, y: lhs.y - rhs)
+}
+
 public extension CGPoint {
     var asSize: CGSize { CGSize(width: x, height: y) }
 }
--- Sources/Essentials/Global/Typealiases.swift
+++ Sources/Essentials/Global/Typealiases.swift
@@ -4,6 +4,7 @@ import UIKit
 public typealias EssentialFont  = UIFont
 public typealias EssentialColor = UIColor
 public typealias EssentialView  = UIView
+public typealias EssentialImage = UIImage
 
 #elseif canImport(AppKit)
 
@@ -11,9 +12,28 @@ import AppKit
 public typealias EssentialFont  = NSFont
 public typealias EssentialColor = NSColor
 public typealias EssentialView  = NSView
+public typealias EssentialImage = NSImage
 
 #endif
 
 public typealias Radians = CGFloat
 
 public let π = CGFloat.pi
+
+import CoreGraphics
+
+public extension EssentialColor {
+    #if os(macOS)
+    convenience init(h: CGFloat = 1.0, s: CGFloat = 1.0, b: CGFloat = 1.0, a: CGFloat = 1.0, colorSpace cgColorSpace: CGColorSpace? = nil) {
+        if let cgColorSpace, let colorSpace = NSColorSpace(cgColorSpace: cgColorSpace) {
+            self.init(colorSpace: colorSpace, hue: h, saturation: s, brightness: b, alpha: a)
+        } else {
+            self.init(hue: h, saturation: s, brightness: b, alpha: a)
+        }
+    }
+    #else
+    convenience init(h: CGFloat = 1.0, s: CGFloat = 1.0, b: CGFloat = 1.0, a: CGFloat = 1.0, colorSpace: CGColorSpace? = nil) {
+        self.init(hue: h, saturation: s, brightness: b, alpha: a)
+    }
+    #endif
+}
